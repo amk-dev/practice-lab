c4fa60404c35dde5067dea17374d47e20610d329
amk-dev <akash.k.mohan98@gmail.com>
2020-07-16 00:18:11 +0530

    refactor: move error span to new ErrorMessage Class

---
+++ src/components/Generic/ErrorMessage.vue
@@ -0,0 +1,21 @@
+<template>
+	
+	<div class="field" v-if="message">
+				
+		<span class="tag is-danger is-light"> {{ message }} </span>
+
+	</div>
+
+</template>
+
+<script>
+	
+	export default {
+
+		name: 'ErrorMessage',
+		props: ['message']
+
+	}
+
+</script>
+
