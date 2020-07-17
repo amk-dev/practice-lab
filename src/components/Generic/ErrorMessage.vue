c044aebf74495dd465780aa2bcbfbdbf46151fb4
amk-dev <akash.k.mohan98@gmail.com>
2020-07-18 02:09:59 +0530

    Add transition to ErrorMessages

--- src/components/Generic/ErrorMessage.vue
+++ src/components/Generic/ErrorMessage.vue
@@ -1,10 +1,12 @@
 <template>
 	
-	<div class="field" v-if="message">
-				
-		<span class="tag is-danger is-light"> {{ message }} </span>
+	<transition name="fade">
+		<div class="field" v-if="message">
+					
+			<span class="tag is-danger is-light"> {{ message }} </span>
 
-	</div>
+		</div>
+	</transition>
 
 </template>
 
@@ -19,3 +21,16 @@
 
 </script>
 
+<style scoped>
+	
+	/* THIS ANIMATION IS A DUPLICATE, USED AT SESSION CHART FOR FLOATINGBOXS  */
+
+	.fade-enter-active, .fade-leave-active {
+		transition: opacity ease .3s;
+	}
+
+	.fade-enter, .fade-leave-to {
+		opacity: 0;
+	}
+
+</style>
